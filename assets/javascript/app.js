4aafc89c1433a7678aa105d70b1d92175d397a4b
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-09-21 11:09:45 -0400

    Adds functions for the ajax call and animating the gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,82 @@
 $(document).ready(function() {
     var topics = ["dog", "cat", "bird", "horse", "snake", "frog", "rabbit", "wolf", "pig", "cow"],
-        $buttonDiv = $("#buttons");
+        $buttonDiv = $("#buttons"),
+        $gifsDiv = $("#gifs");
 
     function createBtns() {
+        //empty the div
+        $buttonDiv.empty();
+
+        //loops through the array of topics
         for (var i = 0; i < topics.length; i++) {
-            var newBtn = $("<button>");
+            //creates a new button elem
+            var newBtn = $("<button>"),
+                animalText = topics[i];
 
-            newBtn.addClass("animalBtn").attr("data-animal", topics[i]).text(topics[i]);
+            //adds attributes and text to the button
+            newBtn.attr({"data-animal": animalText, "class": "animalBtn"}).text(animalText);
 
+            //appends the new button to the DOM
             $buttonDiv.append(newBtn);
         }
     }
 
-    function getGifs() {
-        var apiKey = "fzMl3W1ysk8S6fEQQBU0T8uvRN7cFFf8";
+    function getGifs(animal) {
+        var apiKey = "fzMl3W1ysk8S6fEQQBU0T8uvRN7cFFf8",
+            requestURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=" + apiKey + "&limit=10";
+
+        $.ajax({
+            url: requestURL,
+            method: "GET"
+        }).then(function(response) {
+            //after a successful returned response
+            //empty the div
+            $gifsDiv.empty();
+
+            var respGifs = response.data;
+
+            //Loops through the images returned
+            for (var i = 0; i < respGifs.length; i++) {
+                //creates a new image
+                var newGif = $("<img>"),
+                    gifStill = respGifs[i].images.fixed_width_still.url,
+                    gifActive = respGifs[i].images.fixed_width.url;
+
+                //sets attributes to the new image
+                newGif.attr({"src": gifStill, "data-still": gifStill, "data-active": gifActive, "data-status": "still", "class": "animalGif"});
+
+                //appends the image to the DOM
+                $gifsDiv.append(newGif);                
+            }
+        }).catch(function(error) {
+            //catches the error returned by the ajax call and logs it
+            console.log(`Error: ${error}`);
+        });
     }
+
+    function animateGif() {
+        //gets attributes of the gif that was clicked
+        var selectedGif = $(this),
+            gifStatus = selectedGif.attr("data-status"),
+            stillURL = selectedGif.attr("data-still"),
+            activeURL = selectedGif.attr("data-active");
+
+        //sets the image to animate or still based on current status
+        if (gifStatus === "still") {
+            selectedGif.attr({"src": activeURL, "data-status": "active"});
+        } else {
+            selectedGif.attr({"src": stillURL, "data-status": "still"});
+        }
+    }
+
+    //Event delegation for buttons
+    $(document).on("click", ".animalBtn", function() {
+        //passes attribute value into function
+        getGifs($(this).attr("data-animal"));
+    });
+
+    //Event delegation for animating gifs when clicked
+    $(document).on("click", ".animalGif", animateGif);
 
     createBtns();
 });
